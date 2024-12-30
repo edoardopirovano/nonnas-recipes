4a1e79a5c602fe384ba446d56bebb556cfc97a7f
Edoardo Pirovano <ed@meticulous.ai>
2024-12-30 13:46:35 +0000

    Add Meticulous

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <meta charSet="UTF-8" />
         <title>Rosanna&apos;s Recipes</title>
+        {(process.env.NODE_ENV === "development" || process.env.VERCEL_ENV === "preview") && (
+          // eslint-disable-next-line @next/next/no-sync-scripts
+          <script
+            data-recording-token="nOwUMoLXWiQJsMB9li28JOza8KdzQtUDQ9ZZ1Yl1"
+            data-is-production-environment="false"
+            src="https://snippet.meticulous.ai/v1/meticulous.js"
+          />
+        )}
       </head>
       <body>
         <ApolloProvider>{children}</ApolloProvider>
