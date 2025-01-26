05a48cbe1d8d0e0fa4b59c2b3c3e3a327c7411be
Edoardo Pirovano <ed@meticulous.ai>
2025-01-26 16:03:33 +0000

    Set language in login flow

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,3 +1,14 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerLanguage } from "@/utils/serverTranslation";
 
-export const GET = handleAuth();
+export const GET = handleAuth({
+  login: async (req: NextApiRequest, res: NextApiResponse) => {
+    const language = getServerLanguage();
+    return handleLogin(req, res, {
+      authorizationParams: {
+        ui_locales: language,
+      },
+    });
+  },
+});
